02cbd0273f23f8bc5d5e70f4e181e8758aeba93d
strom314 <jakub.gros777@gmail.com>
2025-02-11 21:21:15 +0100

    add playRound function that plays the round. add viariables that store number of wins add a loop that plays 5 rounds add text that tells the results

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("cc");
 const choices = ["rock", "paper", "scissors"];
+let computerWins = 0;
+let playerWind = 0;
 
 function getComputerChoice() {
     return choices[randomIntFromInterval(0, 2)];
@@ -10,7 +12,42 @@ function randomIntFromInterval(min, max) { // min and max included
 function getHumanChoice() {
     let choice = Number(prompt("choose: rock(1) paper(2) scissors(3)"));
     return choices[choice - 1];
-    
+
 }
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`both players chose ${humanChoice}, it's a draw`);
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("paper beats rock, computer wins");
+        computerWins++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("rocks beats scissors, player wins");
+        playerWind++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("paper beats rock, player wins");
+        playerWind++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("scissors beat paper, computer wins");
+        computerWins++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("rock beats scissors, computer wins");
+        computerWins++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("scissors beat paper, player wins");
+        playerWind++;
+    }
+}
+for (let i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+    // console.log(`player wins: ${playerWind}`);
+    // console.log(`computer wins: ${computerWins}`);
+    // console.log(`round ${i + 1}`);
+}
+
+console.log(`game over \n player won ${playerWind} rounds \n computer won ${computerWins} rounds`);
