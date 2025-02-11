0233ea676892054778d26675f47e4a3f35d3c1e2
strom314 <jakub.gros777@gmail.com>
2025-02-11 21:08:31 +0100

    create human choice function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 console.log("cc");
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
     return choices[randomIntFromInterval(0, 2)];
 }
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+function getHumanChoice() {
+    let choice = Number(prompt("choose: rock(1) paper(2) scissors(3)"));
+    return choices[choice - 1];
+    
+}
 console.log(getComputerChoice());
+console.log(getHumanChoice());
